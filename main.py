07dcf5264943e7badc0bdde511cbfc583135dca2
phoebeachonwa.mehs@gmail.com <phoebeachonwa.mehs@gmail.com>
2018-08-02 22:00:31 -0700

    trying to fix the linking of css to html

---
+++ main.py
@@ -0,0 +1,18 @@
+import webapp2
+import jinja2
+import os
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/aboutme.html")
+        self.response.write(template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
